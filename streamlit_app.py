d4f5bd16c79800d52f07faaf622ee0985251dc97
RAVI SHEKHAR TIWARI <tiwari11.rst@gmail.com>
2021-02-19 19:04:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,29 @@ def train_test_splited(data,split):
     return ( X_train, X_test, y_train, y_test)
 
 #model for Logic regression    
+def model_logicregression_train(parameters):
+    
+    X_train, X_test, y_train, y_test= train_test_splited(Data,Split)   
 
+    clf = LogisticRegression(penalty=parameters['Penality'],max_iter=int(parameters['Max_Iteration']),tol=float(parameters['Tol']))
+    clf=clf.fit(X_train,y_train)
+    prediction=clf.predict(X_test)
+    
+    
+   #model calculate metric
+    accuracy=sklearn.metrics.accuracy_score(y_test,prediction)
+    cm = confusion_matrix(y_test, prediction)
+
+    
+    dict_value={"model":clf,
+                "accuracy": accuracy,
+                 "prediction":prediction,
+                 "Y_actual": y_test,
+                 "Confusion Metric":cm,
+                  "X_test": X_test }
+       
+    
+    return(dict_value)
 #%%
 
 #Body
@@ -109,6 +131,7 @@ if(st.sidebar.button("Click to train the Logistic Regression Model")):
         time.sleep(1)
     st.success("Dataset Loaded")
     
+    model=model_logicregression_train(parameters) 
     
     my_bar = st.progress(0)
     for percent_complete in range(100):
